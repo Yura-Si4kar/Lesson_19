b4a27ce69a8523eb8628952d5d4701aad94463c6
Sichkar Yurii <96392712+Yura-Si4kar@users.noreply.github.com>
2022-05-31 18:46:46 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,100 @@
+$(() => {
+    const STIKERS_URL = 'https://5dd3d5ba8b5e080014dc4bfa.mockapi.io/stickers/';
+
+    const DELETE_BUTTON_CLASS = 'stickers__close';
+    const INPUT_STIKERS_CLASS = 'stickers__text';
+
+    const stickersTemplate = $('#stickersTemplate').html();
+    const $stickersBlock = $('.stickers__block');
+    const $addButton = $('.add_btn');
+
+    let stickersList = [];
+
+    $addButton.on('click', addStickers);
+    $stickersBlock.on('click','.' + DELETE_BUTTON_CLASS, onCloseBtnClick);
+    $stickersBlock.on('focusout','.' + INPUT_STIKERS_CLASS, saveNewStikersText);
+
+    init();
+
+    function init() {
+        getData();
+    }
+
+    function getData() {
+        fetch(STIKERS_URL)
+        .then((res) => res.json())
+        .then((data) => {
+            stickersList = data;
+            renderList(stickersList);        
+        })
+    }
+
+    function addStickers() {
+        const stickers = {
+            description: '',
+        };
+
+        fetch(STIKERS_URL, {
+            method: 'POST',
+            body: JSON.stringify(stickers),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then((res) => res.json())
+        .then((stickers) => {
+            insertStickers(stickers);
+        });
+    }
+
+    function onCloseBtnClick(e) {
+        deleteStickers(e.target.parentElement.dataset.id);
+    }
+
+    function saveNewStikersText(e) {
+        const element = e.target;
+
+        updateStickers(
+            element.parentElement.dataset.id,
+            element.name,
+            element.value,
+        );
+    }
+
+    function updateStickers(id, name, value) {
+        const stickers = stickersList.find((el) => el.id == id);
+
+        stickers[name] = value;
+
+        fetch(STIKERS_URL + id, {
+            method: 'PUT',
+            body: JSON.stringify(stickers),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+    }
+
+    function deleteStickers(id) {
+        stickersList = stickersList.filter((el) => el.id != id);
+
+        fetch(STIKERS_URL + id, {
+            method: 'DELETE',
+        }).then((data) => {
+            getData();
+        });
+    }
+    
+    function insertStickers(stickers) {
+        $stickersBlock.append(generateStickersHtml(stickers));
+    }
+
+    function renderList() {
+        $stickersBlock.html(stickersList.map(generateStickersHtml).join('\n'));
+    }
+
+    function generateStickersHtml(stickers) {
+        return stickersTemplate.replace('{{id}}', stickers.id)
+                                .replace('{{description}}', stickers.description);
+    }
+});
